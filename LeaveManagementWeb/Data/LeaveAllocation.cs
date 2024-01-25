175632a89c4f5fea114b6ae8dc563476af10f37f
pnaraparaju <pnaraparaju@INHYDPNARAPARA9>
2024-01-25 11:23:03 +0530

    Added Database Tables and Migrations. End Implement Database Section

---
+++ LeaveManagementWeb/Data/LeaveAllocation.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementWeb.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        
+        public int NumberOfDays { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+
+        public string EmployeeId { get; set; }
+
+
+
+
+    }
+}
